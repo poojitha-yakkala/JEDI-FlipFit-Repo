035a51c8d240de5f20c4e2022e2d5f9165e70baa
poojitha-yakkala <yakkala.l@flipkart.com>
2025-01-21 12:25:15 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,4 +1,5 @@
 class Demo{
-   public static void main(String args[]);
-   System.out.println("I am Poojitha");
+   public static void main(String args[]){
+       System.out.println("I am Poojitha");
+   }
 }
